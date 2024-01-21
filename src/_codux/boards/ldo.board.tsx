039a1a7b54254d6bfd094a7fd4c4ec27c08ea800
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-01-21 16:46:47 -0300

    pldo
    Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)

---
+++ src/_codux/boards/ldo.board.tsx
@@ -0,0 +1,24 @@
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'ldo',
+    Board: () => 
+    <div>
+        <h1>Term: Budget Guidelines Bill (PLDO)</h1>,
+        <p>Bill initiated by the President of the Republic that comprises 
+        the goals and priorities of the federal public administration, including 
+        capital expenditures for the subsequent financial year. It guides the 
+        preparation of the LOA, provides for changes in tax legislation and 
+        establishes the application policy of official financial development 
+        agencies. The PLDO is sent to the National Congress up to eight and 
+        a half months before the end of the financial year (April 15). 
+        The legislative session will not be interrupted until the PLDO 
+        is approved by the National Congress.</p>
+
+     <p>
+     CF, arts. 57, § 2, and 165, II; ADCT, art. 35, § 2, II.
+     See also: Budget Guidelines Law (LDO).
+     General Concept: Bill (PL) Senate Bill (PLS) , Chamber Bill (PLC)  .</p>
+    </div>,
+    isSnippet: true,
+});
